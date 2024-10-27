d2cc13eb64455938ae2cd3fa3da455fb171ea384
V <victor.chavarrias@deltares.nl>
2024-10-27 14:33:54 +0100

    not updated?

--- optimal_route.py
+++ optimal_route.py
@@ -6,10 +6,16 @@ __all__ = ['find']
 import numpy as np
 from itertools import combinations, permutations #is the best not to use `as`?
 
+
+
+
+
+392.2172595594006  # in kilometers
+
 # FUNCTIONS
 
 def find(points_cord):
-
+    
     (dist_all,comb_idx,npoints) = compute_distances(points_cord)
     (tracks_all,ntracks) = compute_tracks(npoints)
     dist_min = loop_tracks(ntracks,npoints,tracks_all,comb_idx,dist_all)    
@@ -17,7 +23,21 @@ def find(points_cord):
     print(dist_min)
     return
 
+def compute_trip(points_cord):
+
+    npoints = points_cord.shape[0]
+    points_idx = np.arange(0, npoints, 1)
+    comb_idx = np.array(list(combinations(points_idx, 2))) #is the best to convert from combinations to list to array?
+
+    return (npoints,comb_idx)
+
 def compute_distances(points_cord):
+
+    from haversine import haversine, Unit
+
+    #parameters
+    CARTESIAN=1
+    SPHERICAL=2
 
     npoints = points_cord.shape[0]
     points_idx = np.arange(0, npoints, 1)
@@ -26,8 +46,19 @@ def compute_distances(points_cord):
     comb_o_y = points_cord[comb_idx[:,0],1]
     comb_f_x = points_cord[comb_idx[:,1],0]
     comb_f_y = points_cord[comb_idx[:,1],1]
-    dist_all=np.sqrt((comb_f_x-comb_o_x)**2+(comb_f_y-comb_o_y)**2)
 
+    #check if cartesian or spherical
+    cord_type=...
+
+    if cord_type==CARTESIAN:
+        dist_all=np.sqrt((comb_f_x-comb_o_x)**2+(comb_f_y-comb_o_y)**2)    
+    elif cord_type==SPHERICAL:
+        lyon = (45.7597, 4.8422) # (lat, lon)
+        paris = (48.8567, 2.3508)
+        haversine(lyon, paris, Unit.METERS)
+        #check: https://pypi.org/project/haversine/
+        #and compute in matrix form!
+    
     return (dist_all,comb_idx,npoints)
 
 def compute_tracks(npoints):
